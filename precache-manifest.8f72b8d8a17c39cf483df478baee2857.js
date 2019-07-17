310b984ea425493b3e3cb03a00a2d6e2882a3e90
puffski <marcus.karlbowski@autodesk.com>
2019-07-17 22:49:22 +0200

    Updates

---
+++ precache-manifest.8f72b8d8a17c39cf483df478baee2857.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c8657c288f7ff66cc05f9c88847a04a",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "201853b6bc19c27f4497",
+    "url": "/app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ce8669e27bf113e81cf7",
+    "url": "/app/static/js/2.0b485703.chunk.js"
+  },
+  {
+    "revision": "201853b6bc19c27f4497",
+    "url": "/app/static/js/main.08fcbb41.chunk.js"
+  },
+  {
+    "revision": "127917d16c2343d0c8fa",
+    "url": "/app/static/js/runtime~main.d465ad9e.js"
+  }
+]);
